12a125872163e982b3d5b0ea0519a573515700db
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-06-12 17:30:50 -0300

    Creating migrations

---
+++ database/migrations/2020_06_12_173551_create_adresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('zipcode');
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state');
+            // foreing key from table clients
+            $table->unsignedBigInteger('clients_id');
+            $table->foreign('clients_id')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}
---
+++ database/migrations/2020_06_12_202513_create_routes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('codeShipping');
+            $table->string('route');
+            $table->bigInteger('order')->default(1);
+
+            $table->unsignedBigInteger('adresses_id');
+            $table->foreign('adresses_id')->references('id')->on('adresses');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+}
